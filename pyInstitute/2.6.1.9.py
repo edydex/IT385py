51cd38728019966c1bf7d4a361b83304e0f5e58c
edydex <edydex.channel@gmail.com>
2022-01-16 23:53:22 -0800

    Finished with week 2

---
+++ pyInstitute/2.6.1.9.py
@@ -0,0 +1,13 @@
+# input a float value for variable a here
+a=int(input('input a float value for variable "A" here: '))
+# input a float value for variable b here
+b=int(input('input a float value for variable "B" here: '))
+# output the result of addition here
+print(a+b)
+# output the result of subtraction here
+print(a-b)
+# output the result of multiplication here
+print(a*b)
+# output the result of division here
+print(a/b)
+print("\nThat's all, folks!")
