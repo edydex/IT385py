dfae0f17dca3ec019bad0396fde2fa82f8300839
edydex <edydex.channel@gmail.com>
2022-01-20 17:27:14 -0800

    3.1.1.10 lab

---
+++ pyInstitute/3.1.1.10.py
@@ -0,0 +1,9 @@
+word = input("Say Spathiphyllum: ")
+if word == "Spathiphyllum":
+  print("Yes - Spathiphyllum is the best plant ever!")
+elif word == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+elif word == "pelargonium":
+    print("Spathiphyllum! Not pelargonium!")
+else:
+    print("bruh")
