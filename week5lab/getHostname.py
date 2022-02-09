44eb17e120cb83d8f51de87025f31ced479546ac
edydex <edydex.channel@gmail.com>
2022-02-08 18:19:54 -0800

    ClassWorkDone

--- week5lab/getHostname.py
+++ week5lab/getHostname.py
@@ -2,14 +2,17 @@
 
 from pexpect import pxssh
 
-s = pxssh.pxssh()
-hostname = "192.168.0.111"
-username = "justincase"
-password = "Password01"
-s.login(hostname, username, password)
+def get_hostname(hostname):
+    s = pxssh.pxssh()
+    username = "justincase"
+    password = "Password01"
+    s.login(hostname, username, password)
 
-s.sendline('hostname')
-s.prompt()
-print(s.before)
+    s.sendline('hostname')
+    s.prompt()
+    print(s.before)
 
-s.logout
+    s.logout
+ip_addresses = ["192.168.0.121", "192.168.0.122", "192.168.0.111", "192.168.0.112"]
+for ip_address in ip_addresses:
+    get_hostname(ip_address)
