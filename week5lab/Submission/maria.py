2c83e73a34c86e482bce92e831b2e53ebd514acf
edydex <edydex.channel@gmail.com>
2022-02-09 09:46:38 -0800

    maria

--- week5lab/Submission/maria.py
+++ week5lab/Submission/maria.py
@@ -15,12 +15,12 @@ def get_uptime(remote_ip):
     child.sendline("Password01") #in this situation its ok to just type ur pass into terminal unprompted
     child.expect(".*\$")
     #desired changes:
-    child.sendline("sudo apt install apache2 -y") #installing apache (again, takes time)
+    child.sendline("sudo apt install mariadb-server -y") #installing apache (again, takes time)
     child.expect(".*\$")
-    print("apache installed")
-    child.sendline("sudo systemctl enable --now apache2")
+    print("maria installed")
+    child.sendline("sudo systemctl enable --now mariadb")
     child.expect(".*\$")
-    print("apache enabled at start and started")
+    print("mariadb enabled at start and started")
 
     #check for status just in case
     child.sendline("hostname")
@@ -30,6 +30,6 @@ def get_uptime(remote_ip):
     child.sendline("exit")
 
 
-ip_addresses = ["192.168.0.111", "192.168.0.112"]
+ip_addresses = ["192.168.0.121", "192.168.0.122"]
 for ip_address in ip_addresses:
     get_uptime(ip_address)
