ab8e45f87643e1842d1ae30332c62c92e4ce323b
edydex <edydex.channel@gmail.com>
2022-02-08 17:41:25 -0800

    234

--- week5lab/Uptime.py
+++ week5lab/Uptime.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import pexpect
+def get_uptime(remote_ip):
+    child = pexpect.spawn("ssh justincase@" + remote_ip)
+    child.expect(".*password:")
+    child.sendline("Password01")
 
-child = pexpect.spawn("ssh justincase@192.168.0.121")
-child.expect(".*password:")
-child.sendline("Password01")
+    child.expect(".*\$")
+    child.sendline("uptime")
 
-child.expect(".*\$")
-child.sendline("uptime")
+    child.expect(".*\$")
+    print("\n",child.after,"\n")
+    child.sendline("exit")
 
-child.expect(".*\$")
-print("\n",child.after,"\n")
-child.sendline("exit")
+get_uptime("192.168.0.121")
