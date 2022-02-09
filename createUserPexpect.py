ad0c8d1a582d27eed3a7934a4310f3aa0b20e386
edydex <edydex.channel@gmail.com>
2022-02-09 07:39:16 -0800

    222

---
+++ createUserPexpect.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import pexpect
+def get_uptime(remote_ip):
+    #login
+    child = pexpect.spawn("ssh justincase@" + remote_ip)
+    child.expect(".*password:")
+    child.sendline("Password01")
+    print("login phase done")
+
+    child.expect(".*\$")
+    child.sendline("sudo useradd olis")
+    child.expect(".*\:") #expecting sudo pass
+    child.sendline("Password01") #in this situation its ok to just type ur pass into terminal unprompted
+    child.expect(".*\$")
+    child.sendline("sudo passwd olis")
+    child.expect(".*\:") #expecting to type pass
+    child.sendline("p00p")
+    child.expect(".*\:") #expecting to retype pass
+    child.sendline("p00p")
+    print("useradd phase done")
+
+
+
+
+    child.expect(".*\$")
+    print("")
+    child.sendline("exit")
+
+ip_addresses = ["192.168.0.111"] #, "192.168.0.122", "192.168.0.111", "192.168.0.112"]
+for ip_address in ip_addresses:
+    get_uptime(ip_address)
