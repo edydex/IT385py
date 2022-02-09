ac445c2d0d839e778d138e4100bd4c521e07ebb3
edydex <edydex.channel@gmail.com>
2022-02-08 16:18:04 -0800

    1

---
+++ week5lab/connect1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pexpect
+
+child = pexpect.spawn("telnet 192.168.0.11")
+
+child.expect("Username:")
+child.sendline("cisco")
+child.expect("Password:")
+child.sendline("cisco")
+
+child.expect("IT385-CSR1")
+child.sendline("conf t")
+
+child.expect("IT385-CSR1")
+child.sendline("int g2")
+child.expect("IT385-CSR1")
+child.sendline("ip addr 10.10.10.2 255.255.255.0")
+child.expect("IT385-CSR1")
+child.sendline("desc try 2")
+child.expect("IT385-CSR1")
+child.sendline("no shut")
+
+child.expect("IT385-CSR1")
+child.sendline("exit")
+child.expect("IT385-CSR1")
+child.sendline("exit")
+child.expect("IT385-CSR1")
+child.sendline("exit")
