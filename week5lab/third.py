f5986555c11bcc33a78adf6a61624aa07b4ed09a
edydex <edydex.channel@gmail.com>
2022-02-09 07:23:55 -0800

    Merge branch 'master' of https://github.com/edydex/IT385py

--- week5lab/third.py
+++ week5lab/third.py
@@ -3,17 +3,27 @@
 from pexpect import pxssh
 #define login function
 def newuser(hostname):
+    #login phase
     s = pxssh.pxssh()
     username = "justincase"
     password = "Password01"
     s.login(hostname, username, password)
-
+    print("passed login phase")
+    #useradd phase
     s.sendline('sudo useradd olis')
     s.prompt()
-    print(s.before)
-
+    #in this situation it won't hurt to put password in
+    s.sendline('Password01')
+    s.sendline('sudo passwd olis')
+    s.prompt()
+    s.sendline('p00p')
+    s.prompt()
+    s.sendline('p00p')
+    s.prompt()
+    print("passed password phase")
+    #i guess this works but extremely slow
     s.logout
 
-ip_addresses = ["192.168.0.121", "192.168.0.122", "192.168.0.111", "192.168.0.112"]
+ip_addresses = ["192.168.0.111"] #"192.168.0.122", "192.168.0.111", "192.168.0.112"]
 for ip_address in ip_addresses:
     newuser(ip_address)
