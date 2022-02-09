d62cc2ba776dc9b53c19ee37b0adbc7c2a531ee2
edydex <edydex.channel@gmail.com>
2022-02-08 18:12:28 -0800

    2221

--- week5lab/getHostname.py
+++ week5lab/getHostname.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 from pexpect import pxssh
+
 s = pxssh.pxssh 
 hostname = "192.168.0.111"
 username = "justincase"
 password = "Password01"
-
 s.login(hostname, username, password)
 
-s.sendline("hostname")
+s.sendline('hostname')
 s.prompt()
 print(s.before)
 
