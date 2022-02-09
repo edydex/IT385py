e697e9db91e4903052b7c78c9fd60bef532d7924
edydex <edydex.channel@gmail.com>
2022-02-09 09:47:52 -0800

    nomaria

---
+++ week5lab/Removers(stabilizers)/nomaria.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import pexpect
+def get_uptime(remote_ip):
+    #login
+    child = pexpect.spawn("ssh justincase@" + remote_ip)
+    child.expect(".*password:")
+    child.sendline("Password01")
+    print("login phase done")
+    #creating user
+    child.expect(".*\$")
+    child.sendline("sudo apt remove mariadb -y")
+    child.expect(".*\:") #expecting sudo pass
+    child.sendline("Password01") #in this situation its ok to just type ur pass into terminal unprompted
+    print("removed apache")
+    #Hostnames just in case
+    child.expect(".*\$")
+    child.sendline("hostname")
+
+    child.expect(".*\$")
+    print("\n",child.after,"\n")
+    child.sendline("exit")
+
+
+ip_addresses = ["192.168.0.111", "192.168.0.112"]
+for ip_address in ip_addresses:
+    get_uptime(ip_address)
